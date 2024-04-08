3039b06794a1d09e8bef63290b2708947adf9e9f
shad <shad@fmpdev.net>
2024-04-08 17:36:59 +0500

    fixing

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.get("/", (req, res) => {
-  res.send("App is running, version: 0.0.5");
+  res.send("App is running, version: 0.0.6");
 });
 
 if (process.env.NODE_ENV == "development") {
