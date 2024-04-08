f5f7abf4e107d67240e3183d093277527dbad11b
shad <shad@fmpdev.net>
2024-04-09 01:58:10 +0500

    update version

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 app.get("/", (req, res) => {
-  res.send("App is running, version: 0.0.7");
+  res.send("App is running, version: 0.0.8");
 });
 
 if (process.env.NODE_ENV == "development") {
